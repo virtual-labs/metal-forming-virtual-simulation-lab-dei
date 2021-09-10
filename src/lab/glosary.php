a0d86de367c049edd02b9e7dc134eaf14ba2057b
ajaykant00 <ajaykant00@gmail.com>
2021-09-10 12:30:14 +0530

    Code Updated

---
+++ src/lab/glosary.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML public "-w3c//dtd//xhtml 1.0 transitional//en" "http://www.w3.org/tr/xhtml1/dtd/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="shortcut icon" type="image/x-icon" href="images/icon.ico">
+<link href="css/main.css" media="all" rel="stylesheet" type="text/css">
+<link href="css/dropdown.css" media="all" rel="stylesheet" type="text/css">
+<link href="css/advanced.css" media="all" rel="stylesheet" type="text/css">
+<link href="css/pagination.css" media="all" rel="stylesheet" type="text/css">
+<SCRIPT language="javascript">
+msg = "Virtual Lab - Dayalbagh Educational Institute ";
+msg = msg;pos = 0;
+function scrollMSG() {
+document.title = msg.substring(pos, msg.length) + msg.substring(0, pos);
+pos++;
+if (pos >  msg.length) pos = 0
+window.setTimeout("scrollMSG()",200);
+}
+scrollMSG();
+</SCRIPT>
+</head>
+<body bgcolor="#FFFFFF">
+<div id="header_main"></div>
+<div id="header">
+<ul class="dropdown dropdown-horizontal">
+<?php
+include("mainmenu.php");
+?>
+</ul></div>
+<script>
+// Google Analytic Code
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57409344-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<div style="background-image:url(images/content_bg.jpg);position: relative; margin:auto; width: 1024px; min-height:550px;">
+<div class="box">
+<h2>Glossary</h2><br />
+<span class="blue">2D</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Object or drawing showing or having two dimensions, so that the object or drawing is lacking depth.<br /><br />
+<span class="blue">3D</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;3D - Object or drawing showing or having three dimensions, so that the object or drawing is showing a depth dimension.<br /><br />
+<span class="blue">AISI</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Acronym for American Iron and Steel Institute.<br /><br />
+<span class="blue">ASTM</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;American Standard of Testing and Materials.<br /><br />
+<span class="blue">Axisymmetric</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Deformation models require that the deforming geometry be symmetric around a central axis.<br /><br />
+<span class="blue">Axisymmetric drawing</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;The drawing of shapes having an axis of symmetry such as cones and round cups.<br /><br />
+<span class="blue">Bendability</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;It is defined as the minimum bending radius attainable by a given material.<br /><br />
+<span class="blue">Bending</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;A term typically applied to a metal forming process. It is the creation of a formed feature by angular displacement of a sheet metal workpiece.<br /><br />
+<span class="blue">Billet</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Billet is the product obtained from a further reduction by hot rolling, (cross sectional area &gt 40x40 mm<sup>2</sup>). <br><br><center><img src="images/MetalForming/billet.jpg" width="400" height="200"><br>Figure: 1 - Billet</center><br />
+<span class="blue">Blanking</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;The operation of punching, cutting, or shearing a piece out of stock to a predetermined shape. The article punch out is called blank.<br /><br />
+<span class="blue">Blocking</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Another coating defect consisting of the adhesion of two adjoining coatings or materials. Usually this term refers to the coating on one side of coated plate being tacky or sticky and adhering to the adjacent sheet.<br /><br />
+<span class="blue">Bloom</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Bloom is the product of first breakdown of ingot (cross sectional area &gt; 230 cm<sup>2</sup>).<br><br><center><img src="images/MetalForming/bloom.jpg" width="400" height="200"><br>Figure: 2 - Bloom</center><br />
+<span class="blue">Bolster plate</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;That plate to which dies can be fastened so the assembly is secured to the top surface of a press bed.<br /><br />
+<span class="blue">Bottoming</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Forming operation in which the punch and the die are closed completely on the workpiece.<br /><br />
+<span class="blue">Buckling</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;An uncontrolled deformation pattern perpendicular to the surface of a sheet caused by compressive stresses.<br /><br />
+<span class="blue">Bulging</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;The process of increasing the diameter of a cylindrical shell (usually to a spherical shape) or of expanding the outer walls of any shell or box shape whose walls were previously straight.<br /><br />
+<span class="blue">Closed-die</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;A tool that creates a work-shape-imposing orifice, cavity, or passageway.<br /><br />
+<span class="blue">Coining</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;A compressive metal flowing action. A closed-die squeezing operation in which all surfaces of a workpiece are confined or restrained, resulting in a well-defined imprint of the die on the work.<br /><br />
+<span class="blue">Cold heading</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;The process of upsetting the ends of a bar, wires, or tube stock while cold.<br /><br />
+<span class="blue">Cold rolled</span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;Rolling metal at a temperature below the recrystallization temperature point of the metal to create strain hardening (work-hardening).
+</div></div>
+<div id="footer">
+&copy; Metal Forming Virtual Simulation Lab - Dayalbagh Educational Institute (www.dei.ac.in)
+</div>
+</body>
+</html>
